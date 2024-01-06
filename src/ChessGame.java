7f2a32e8cfee2041b9e3354c86b83558e7338ff8
sahilreddy <25sreddy@athenian.org>
2024-01-05 22:29:25 -0800

    mouse pressed works

--- src/ChessGame.java
+++ src/ChessGame.java
@@ -4,6 +4,7 @@ import jdk.jfr.StackTrace;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.util.ArrayList;
 import java.awt.event.MouseMotionAdapter;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -21,13 +22,7 @@ import java.awt.*;
  *
  * @author Khouiled
  */
-class ChessPiece extends JLabel {
-    private int offsetX, offsetY;
 
-    public ChessPiece(String pieceImage) {
-        super(new ImageIcon(pieceImage));
-        setSize(60, 60); // Assuming each piece has a size of 60x60
-    }
 
 
 }
@@ -40,13 +35,22 @@ public class ChessGame extends JLabel {
         addMouseListener(new MouseAdapter() {
             @Override
             public void mousePressed(MouseEvent e) {
-                offsetX = e.getX();
-                offsetY = e.getY();
-                System.out.println(offsetX);
-                System.out.println(offsetY);
-                Component component = getComponentAt(e.getPoint());
-                if (component instanceof ChessPiece) {
-                    draggedPiece = (ChessPiece) component;
+                System.out.println("pressed");
+                offsetX = e.getX()%90;
+                offsetY = e.getY()%90;
+                Point point = new Point(e.getX(), e.getY());
+                System.out.println("X Offset: " + offsetX);
+                System.out.println("Y Offset: " + offsetY);
+                System.out.println("X: " + e.getX());
+                System.out.println("Y: " + e.getY());
+                //get square--
+                original_square = getSquareIndex(point);
+                System.out.println("Square: " + original_square);
+                //get piece on square
+                int pieceVal = Main.getPieceOn(original_square);
+                String imagePath = "src/";
+                if (pieceVal/8 == 1) {
+                    imagePath += "White ";
                 }
             }
 
@@ -55,21 +59,80 @@ public class ChessGame extends JLabel {
                 if (draggedPiece != null) {
                     int fromSquare = getSquareIndex(draggedPiece.getLocation());
                     int toSquare = getSquareIndex(e.getPoint());
-
                     // Update main.allBoards based on the drag-and-drop
                     // (You may need to adjust this logic based on your piece representation)
                     int pieceIndex = 0; // Change this to determine the piece type
-                    //https://stackoverflow.com/questions/1073318/in-java-is-it-possible-to-clear-a-bit
-                    long fromMask = 1L << fromSquare;
-                    long toMask = 1L << toSquare;
-                    // Clear the from bit
-                    Main.allBoards[pieceIndex].board &= ~fromMask;
-                    // Set the to bit
-                    Main.allBoards[pieceIndex].board |= toMask;
+                    int pieceVal = Main.getPieceOn(original_square);
+                    pieceIndex = pieceVal % 8;
+                    // create a short that represents the move
+                    short move = 0;
+                    if (Main.allBoards[7].get(toSquare) != 0) {
+                        short mask = (short)0b1000000000000000;
+                        move = (short)(move | mask);
+                    }
+                    if (pieceIndex == 1 && move == 0 && (Math.abs(original_square-toSquare) == 7 || Math.abs(original_square-toSquare) == 9)) {
+                        //pawn that didn't capture
+                        short mask = (short)0b1111000000000000;
+                        move = (short)(move | mask);
+                    }
+                    else if (pieceIndex == 1 && (toSquare >= 56 || toSquare <=7)) {
+                        //pawn moving to vertical edge of board
+                        short mask = (short)0b0110000000000000;
+                        move = (short)(move | mask);
+                    }
+                    else if (pieceIndex == 2){
+                        short mask = (short)0b0001000000000000;
+                        move = (short)(move | mask);
+                    }
+                    else if(pieceIndex == 3){
+                        short mask = (short)0b0010000000000000;
+                        move = (short)(move | mask);
+                    }
+                    else if(pieceIndex == 4){
+                        short mask = (short)0b0011000000000000;
+                        move = (short)(move | mask);
+                    }
+                    else if(pieceIndex == 5){
+                        short mask = (short)0b0100000000000000;
+                        move = (short)(move | mask);
+                    }
+                    else if(pieceIndex == 6){
+                        short mask = (short)0b0101000000000000;
+                        move = (short)(move | mask);
+                    }
+                    short mask = (short)(original_square << 6);
+                    move = (short)(move | mask);
+                    mask = (short)(toSquare);
+                    move = (short)(move | mask);
+                    // compare it to the allowedMoves
+                    // make move if its allowed
+                    if (allowedMoves.contains(move)) {
+                        // Clear the from bit
+                        Main.allBoards[pieceIndex].board = setBit(Main.allBoards[pieceIndex].board, 63 - fromSquare, (short) 0);
+                        // Set the to bit
+                        for (int i = 1; i < 7; i++) {
+                            short b = 0;
+                            if (i == pieceIndex) {
+                                b = 1;
+                            }
+                            Main.allBoards[i].board = setBit(Main.allBoards[i].board, 63 - toSquare, b);
+                        }
 
-                    draggedPiece.setLocation((7 - toSquare / 8) * 90, (toSquare % 8) * 90);
+
+                        //edit the color bitboard
+                        Main.allBoards[0].board = setBit(Main.allBoards[0].board, 63 - fromSquare, (short) 0);
+                        Main.allBoards[0].board = setBit(Main.allBoards[0].board, 63 - toSquare, (short) (pieceVal / 8));
+                        Main.moveInfo = MoveInfo.getMoveInfo(move, Main.moveInfo);
+                        allowedMoves = PrecomputedData.generateMoves(Main.allBoards, Main.moveInfo);
+                        for (short moves : allowedMoves) {
+                            System.out.println(MoveInfo.moveTranslator(moves));
+                        }
+                    }
+                    x = (7 - toSquare / 8) * 90;
+                    y = (toSquare % 8) * 90;
                     draggedPiece = null;
-                    repaint();
+                    chessboardPanel.repaint();
+                    System.out.println("released" + "x cord:" + x + "y cord:" + y + "toSquare:" + toSquare);
                 }
             }
         });
@@ -87,7 +150,7 @@ public class ChessGame extends JLabel {
     }
 
     private int getSquareIndex(Point point) {
-        int row = 7 - point.y / 90;
+        int row = point.y / 90;
         int col = point.x / 90;
         return row * 8 + col;
     }
@@ -173,6 +236,7 @@ public class ChessGame extends JLabel {
         frame.add(chessboardPanel);
         frame.setDefaultCloseOperation(3);
         frame.setVisible(true);
+
 
     }
     public static void main(String[] args) {
